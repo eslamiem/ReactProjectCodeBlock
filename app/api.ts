6b065ceaa9b8feed1004e40e5f803fa27a8dc65d
Marzieh Eslami <eslami.marzieh@gmail.com>
2025-11-26 09:31:51 -0800

    add login page

--- app/api.ts
+++ app/api.ts
@@ -1,11 +1,14 @@
 "use server";
 import { prisma } from "@/database";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function createBlock(formData: FormData) {
   const title = formData.get("title") as string;
   const code = formData.get("code") as string;
-  const block = await prisma.block.create({ data: { title, code } });
+  // Create the new block in the database
+  const userId = Number((await cookies()).get("user_id")?.value);
+  const block = await prisma.block.create({ data: { title, code, userId } });
   redirect("/");
 }
 
@@ -33,4 +36,21 @@ export async function deleteBlock(id: number, formData: FormData) {
 
   // Redirect home
   redirect("/");
+}
+
+export async function handleLogin(formData: FormData) {
+  const username = formData.get("username") as string;
+  const password = formData.get("password") as string;
+  try {
+    const foundUser = await prisma.user.findUniqueOrThrow({
+      where: { username, password },
+    });
+    if (!foundUser) {
+      redirect("/login");
+    } else {
+      (await cookies()).set("user_id", String(foundUser.id));
+      redirect("/");
+    } 
+  } catch (error: any) {
+    redirect(`/login?error=${error.message}`);  } 
 }
